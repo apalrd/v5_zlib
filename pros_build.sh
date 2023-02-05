b10676f768cafbef24e68a2a301876bc3b432796
Andrew Palardy <apalrd@gmail.com>
2023-02-05 11:20:26 -0500

    Update readme and version

--- pros_build.sh
+++ pros_build.sh
@@ -18,4 +18,4 @@ mkdir -p include/zlib
 cp zlib.h zconf.h ./include/zlib/
 
 #Tell Pros to generate a library
-prosv5 c create-template . libz 1.0.0 --system "include/zlib/zlib.h" --system "include/zlib/zconf.h" --system "firmware/libz.a" --target v5
+prosv5 c create-template . libz 1.2.13 --system "include/zlib/zlib.h" --system "include/zlib/zconf.h" --system "firmware/libz.a" --target v5
